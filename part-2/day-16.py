e0179188c3600c6416b6efa62f0ffc9f424397f6
adimond24 <adimond24@s.tooeletech.edu>
2024-09-24 21:48:04 -0600

    part 1

--- part-2/day-16.py
+++ part-2/day-16.py
@@ -1,5 +1,5 @@
 # from turtle import Turtle, Screen
-
+from numpy.ma.core import right_shift
 # timmy = Turtle()
 # print(timmy)
 # timmy.shape("turtle")
@@ -8,4 +8,12 @@
 # my_screen = Screen()
 # print(my_screen.canvheight)
 # my_screen.exitonclick(
-# 
+#
+
+from prettytable import PrettyTable
+table = PrettyTable()
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electic", "fire", "water"])
+table.align = "l"
+print(table)
+
